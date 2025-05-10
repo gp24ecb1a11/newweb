be92e88cad211da16a483fe0afb3742d920bac49
gp24ecb1a11 <gp24ecb1a11@student.nitw.ac.in>
2025-05-10 17:21:56 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAeOS8_0tDWKnfAwLf0GRKr6JaopYj1nnY",
+    authDomain: "dormdash-40a10.firebaseapp.com",
+    databaseURL: "https://dormdash-40a10-default-rtdb.asia-southeast1.firebasedatabase.app", // ✅ UPDATED
+    projectId: "dormdash-40a10",
+    storageBucket: "dormdash-40a10.appspot.com",
+    messagingSenderId: "219135353050",
+    appId: "1:219135353050:web:49446a2e74414ebf8105e3"
+};
+// ✅ Initialize Firebase
+firebase.initializeApp(firebaseConfig);
